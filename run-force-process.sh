7b07fe7e9631ed4ae8cfeaa7ff2c2e8feec7eec1
Lion <lion@playhyp.com>
2026-01-10 23:16:55 +0000

    Remove remaining automation code files from archive repository

--- run-force-process.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-export GIT_REPO_PATH="$HOME/hytale-launcher-archives"
-cd ~/hytale-launcher-archives
-node force-process-current.js
